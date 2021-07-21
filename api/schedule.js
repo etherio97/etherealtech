c03e308ba45cd704b437e61b75c9d5b0e1ecf4d0
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-21 21:11:43 +0630

    Update schedule.js

--- api/schedule.js
+++ api/schedule.js
@@ -1,14 +1,23 @@
+const { APP_SECRET } = process.env;
+
 module.exports = (req, res) => {
   const { token } = req.query;
   
-  if (!token) {
-    return res.status(403).json({
-      status: 'forbidden',
-      code: 403,
-    });
-  }
+  if (!token) return res.status(401).json({
+    code: 401,
+    status: 'error',
+    message: 'Unauthorized',
+  });
+  
+  if (token !== APP_SECRET) return res.status(403).json({
+    code: 403,
+    status: 'error',
+    message: 'Forbidden',
+  });
   
   res.json({
-    text: 'x',
+    code: 200,
+    status: 'success',
+    message: 'OK',
   });
 };
