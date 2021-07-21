cd1a502091a269d7950ec8e8b47e69b0f65b144b
Etherio <50423290+etherio97@users.noreply.github.com>
2021-07-21 22:48:40 +0630

    Update schedule.js

--- api/schedule.js
+++ api/schedule.js
@@ -1,7 +1,9 @@
 const { APP_SECRET, DATABASE_TOKEN, GITHUB_TOKEN } = process.env;
 const { default: axios } = require('axios');
 
+const GITHUB_WORKFLOW_ID = '11461024';
 const REQUEST_URL = 'https://api.github.com/repos/etherealtech/aungsan-live-schedule/actions/workflows/11461024/dispatches';
+
 class DB {
     static BASE_URL = 'https://us1-merry-cat-32748.upstash.io';
 
@@ -16,20 +18,54 @@ class DB {
     }
 }
 
+class Actions {
+    static BASE_URL = 'https://github.com/repos/etherealtech/aungsan-live-schedule/actions';
+    static REST_API = 'https://api.github.com/repos/etherealtech/aungsan-live-schedule/actions';
+    
+    static dispatch(id, inputs) {
+      return axios.post(`${this.REST_API}/workflows/${id}/dispatches`,  {
+        ref: 'refs/heads/main',
+        inputs
+      }, { 
+        headers:{
+          'content-type': 'application/json',
+          'authorization': 'token ' + GITHUB_TOKEN
+        }
+      });
+    }
+    
+    static async runs(limit = null) {
+        const { data } = await axios.get(`${this.REST_API}/runs?limit=${limit || 10}`);
+        const actions = data['workflow_runs'] || [];
+        return actions.filter(({ event }) => event === 'workflow_dispatch');
+    }
+}
+
 module.exports = async (req, res) => {
   const { token } = req.query;
   if (!token) return res.status(401).json({ status: 401, error: 'Unauthorized' });
   if (token !== APP_SECRET) return res.status(403).json({ status: 403, error: 'Forbidden' });
-  const headers = {
-    'content-type': 'application/json',
-    'authorization': 'token ' + GITHUB_TOKEN,
-  };
-  const { status, data } = await axios.post(REQUEST_URL,  {
-    ref: 'refs/heads/main',
-    inputs: {
-      cron_schedule: '0 0 19 * * *'
+  try {
+    let [ run ] = await Actions.runs();
+    let { id, status, conclusion, html_url, created_at, updated_at } = run;
+
+    switch(status) {
+      case 'queued':
+      case 'in_progress':
+        return res.staus(400).json({ status: 400, error: 'Bad Request', message: html_url });
+      case 'completed':
+        break;
+      default:
+        console.log('[Unexcepted Run Status] %s', status);
     }
-  }, { headers });
-  
-  res.json({ status, data });
+    
+    await Actions.dispatch(GITHUB_WORKFLOW_ID, {
+      cron_schedule: '0 0 19 * * *'
+    });
+    
+    res.json({ status: 200, message: Actions.BASE_URL });
+  } catch(e) {
+    console.error('[ERR]', e);
+    res.status(500).json({ error: e.message });
+  }
 };
